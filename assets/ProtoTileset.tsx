2818025320c717165af97efc2829f68a2c8981d0
Xifiggam <xifiggam@gmx.at>
2017-12-15 22:21:53 +0100

    Prototyleset / Map

---
+++ assets/ProtoTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Proto2Tilest" tilewidth="128" tileheight="128" tilecount="4" columns="2">
+ <image source="tileset.png" width="256" height="256"/>
+</tileset>
