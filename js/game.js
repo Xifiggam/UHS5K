4ce02c916e98fae41a919a2d93576d03a89a7615
lukas <l.knoch@gmx.at>
2017-12-15 22:50:44 +0100

    zooming is wonky but working

--- js/game.js
+++ js/game.js
@@ -16,10 +16,12 @@ var gameState = {
         game.world.setBounds(0, 0, 2000, 2000);
         this.map = game.add.tilemap(ASSETS.TILES_DESERT_JSON);
 
+        this.gameWorld = game.add.group();
+        this.gameWorld.position.setTo(game.world.centerX, game.world.centerY)
+
         this.map.addTilesetImage('Desert', ASSETS.TMW_DESERT_SPACING);
 
         this.layer = this.map.createLayer('Ground');
-
         this.layer.resizeWorld();
         this.cursors = game.input.keyboard.createCursorKeys();
         this.zoomButtons = game.input.keyboard.addKeys({'in': Phaser.KeyCode.L, 'down': Phaser.KeyCode.K});
@@ -46,11 +48,34 @@ var gameState = {
             game.camera.x += 4;
         }
         if (this.zoomButtons.in.isDown) {
+
+            var centerIn = Phaser.Point.add(game.camera.position,
+                new Phaser.Point(game.camera.view.halfWidth, game.camera.view.halfHeight));
+
+            var oldCameraScaleIn = game.camera.scale.clone();
+
             game.camera.scale.x += this.ZOOM_FACTOR;
             game.camera.scale.y += this.ZOOM_FACTOR;
+
+
+            var cameraScaleRatioIn = Phaser.Point.divide(game.camera.scale, oldCameraScaleIn);
+            game.camera.focusOn(Phaser.Point.multiply(centerIn, cameraScaleRatioIn));
+
+
         } else if (this.zoomButtons.down.isDown) {
+
+            var center = Phaser.Point.add(game.camera.position,
+                new Phaser.Point(game.camera.view.halfWidth, game.camera.view.halfHeight));
+
+            var oldCameraScale = game.camera.scale.clone();
+
             game.camera.scale.x += -this.ZOOM_FACTOR;
             game.camera.scale.y += -this.ZOOM_FACTOR;
+
+
+            var cameraScaleRatio = Phaser.Point.divide(game.camera.scale, oldCameraScale);
+            game.camera.focusOn(Phaser.Point.multiply(center, cameraScaleRatio));
+
         }
     },
 
