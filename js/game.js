deeb6a2004b7fdc3c56afcddfdbe51695d589288
Zelle <christian@zellot.at>
2017-12-16 14:40:19 +0100

    build rectangle

--- js/game.js
+++ js/game.js
@@ -14,8 +14,11 @@ var gameState = {
     stars: [],
     layers: [],
 
+    buildMarker: null,
+    buildTile: null,
+    
+
     create: function () {
-console.log("now");
         this.cursors = game.input.keyboard.createCursorKeys();
 
         this.map = game.add.tilemap(ASSETS.TILES_PROTO_KARTE);
@@ -24,27 +27,32 @@ console.log("now");
         this.onLayers(function(layer){
             layer.resizeWorld();
         });
-
-
         this.createNewsHud();
+
+
         this.setupStarsHud();
         this.setupMoneyHud(1000);
+
 
         this.cursors = game.input.keyboard.createCursorKeys();
         this.zoomButtons = game.input.keyboard.addKeys({'in': Phaser.KeyCode.L, 'down': Phaser.KeyCode.K});
         this.miscButtons = game.input.keyboard.addKeys({'shake': Phaser.KeyCode.O});
 
         game.input.onDown.add(this.addObject, this);
+        this.activeBuildCursor(2, 1, false);
     },
 
     update: function () {
         this.inputHandling();
+        this.updateBuildCursor();
     },
 
     createLayers: function () {
         this.layers.push(this.map.createLayer('Floor'));
         this.layers.push(this.map.createLayer('Wall'));
         this.layers.push(this.map.createLayer('Object'));
+        this.objectLayer = this.layers[2];
+        this.wallLayer = this.layers[1];
     },
 
     onLayers: function(func){
@@ -75,12 +83,12 @@ console.log("now");
     },
 
     addObject: function (point) {
-        console.log(point);
-        console.log(this.map);
-        var posX = (this.layers[0].getTileX(point.worldX) * TILE.SIZE);
-        var posY = (this.layers[0].getTileY(point.worldY) * TILE.SIZE);
-        var sprite = this.game.add.sprite(posX, posY, ASSETS.PERSON);
-        sprite.scale.setTo(0.25,0.25);
+        // console.log(point);
+        // console.log(this.map);
+        // var posX = (this.layers[0].getTileX(point.worldX) * TILE.SIZE);
+        // var posY = (this.layers[0].getTileY(point.worldY) * TILE.SIZE);
+        // var sprite = this.game.add.sprite(posX, posY, ASSETS.PERSON);
+        // sprite.scale.setTo(0.25,0.25);
 
         this.starsVisible(Math.random()*5);
         this.setupMoneyHud(Math.random()*1000);
@@ -103,6 +111,8 @@ console.log("now");
         text.anchor.set(0);
         text.fixedToCamera = true;
     },
+
+    
     postNews: function (text,delay) {
         var style = { font: "14px Arial", fill: "#FFFFFF", align: "left" };
         var text = game.add.text(15, 45, text, style);
@@ -140,6 +150,61 @@ console.log("now");
         for (var i = 0; i < this.stars.length; i++) {
             var star = this.stars[i];
             star.alpha = count < i;// <= this is my favourite line of code.
+        }
+    },
+
+    
+    activeBuildCursor: function(width, height, collision){
+        this.buildMarker = game.add.graphics();
+        var color = collision ? 0xFF0000 : 0x000000;
+        this.buildMarker.lineStyle(2, color, 1);
+        this.buildMarker.collision = collision;
+        this.buildMarker.drawRect(0, 0, TILE.SIZE * width, TILE.SIZE * height);
+        this.buildMarker.u_width = width;
+        this.buildMarker.u_height = height;
+
+    },
+
+
+    updateBuildCursor: function(){
+        if(this.buildMarker !== null) {
+
+            var x = this.objectLayer.getTileX(game.input.activePointer.worldX) * TILE.SIZE;
+            var y = this.objectLayer.getTileY(game.input.activePointer.worldY) * TILE.SIZE;
+            var collide = false;
+            for (var width = x; width < (this.buildMarker.u_width*TILE.SIZE + x ); width += TILE.SIZE) {
+                for(var height = y; height < (this.buildMarker.u_height*TILE.SIZE + y); height += TILE.SIZE){
+                    collide = collide || this.map.getTile(this.objectLayer.getTileX(width), this.objectLayer.getTileY(height), this.objectLayer) != null;
+                    collide = collide || this.map.getTile(this.wallLayer.getTileX(width), this.wallLayer.getTileY(height), this.wallLayer) != null;
+                    if(collide) {
+                        break;
+                    }
+                }
+            }
+            if(collide) {
+                if(!this.buildMarker.collision) {
+                    this.buildMarker.destroy();
+                    this.activeBuildCursor(this.buildMarker.u_width, this.buildMarker.u_height, true);
+                }
+            } else {
+                if(this.buildMarker.collision){
+                    this.buildMarker.destroy();
+                    this.activeBuildCursor(this.buildMarker.u_width, this.buildMarker.u_height, false);
+                }
+            }
+            this.buildMarker.x = x;
+            this.buildMarker.y = y;
+            if (game.input.mousePointer.isDown)
+            {
+
+
+            //     else
+            // {
+            //     if (this.map.getTile(this.objectLayer.getTileX(this.buildMarker.x), this.objectLayer.getTileY(this.buildMarker.y)).index != this.buildTile.index)
+            //     {
+            //         this.map.putTile(this.buildTile, this.objectLayer.getTileX(this.buildMarker.x), this.objectLayer.getTileY(this.buildMarker.y));
+            //     }
+            }
         }
     }
 };
