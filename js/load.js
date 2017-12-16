abd59de2d43d218d15886ca393a30909378a2237
Zelle <christian@zellot.at>
2017-12-16 19:54:21 +0100

    fix tv

--- js/load.js
+++ js/load.js
@@ -22,7 +22,7 @@ var loadState = {
         game.load.image(ASSETS.SHOWER, 'assets/img/shower_64x64.png');
         game.load.image(ASSETS.TOILET, 'assets/img/toilet_64x32.png');
         game.load.image(ASSETS.SINK, 'assets/img/sink_32x32.png');
-        game.load.image(ASSETS.TV, 'assets/img/star.png');
+        game.load.image(ASSETS.TV, 'assets/img/TV.png');
 
         game.load.tilemap(ASSETS.TILES_PROTO_KARTE, 'assets/HotelMap.json', null, Phaser.Tilemap.TILED_JSON);
         game.load.image(ASSETS.TILESET_PROTO_KARTE, 'assets/tileset.png');
